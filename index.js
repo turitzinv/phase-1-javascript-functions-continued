59a8284535a598d219f6629d7704a37b46818830
Victor Turitzin <turitzinv@gmail.com>
2021-11-26 13:02:35 -0500

    commit tree 7d352fa542cc34820563083c7c9c5532b894b675

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-function saturdayFun(activity="roller-skate") {
-  return `This Saturday, I want to ${activity}!`
+function saturdayFun (activity = `roller-skate`) {
+  return `This Saturday, I want to ${activity}!`;
 }
 
-let mondayWork = function(activity="go to the office") {
-  return `This Monday, I will ${activity}.`
+let mondayWork = function (activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
 }
 
-let wrapAdjective = function(style="*") {
-  return function(adjective="special") {
-    return `You are ${style}${adjective}${style}!`
-  }
+let wrapAdjective = function (visualFlair = "*") {
+  return function (parameter = "special") {
+    return `You are ${visualFlair}${parameter}${visualFlair}!`
+  };
 }
